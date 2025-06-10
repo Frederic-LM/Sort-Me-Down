a2a781ff3972e7a51cd1620e3747897bad2d4556
Frederic-LM <59538680+Frederic-LM@users.noreply.github.com>
2025-06-11 01:52:04 +0200

    Update bangbang.py

--- bangbang.py
+++ bangbang.py
@@ -88,7 +88,7 @@ class Config:
         self.SOURCE_DIR = Path("C:/temp")          # Replace with your media download directory
         self.MOVIES_DIR = Path("B:/Movies")        # Replace with your Movies directory
         self.TV_SHOWS_DIR = Path("B:/Series")      # Replace with your TV Shows directory
-        self.ANIME_MOVIES_DIR = Path("B:/Anime_Movies) # Replace with your Anime Movies directory
+        self.ANIME_MOVIES_DIR = Path("B:/Anime_Movies") # Replace with your Anime Movies directory
         self.ANIME_SERIES_DIR = Path("B:/Anime_TV")# Replace with your Anime Series directory
         
         # API Configuration
