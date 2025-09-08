27375f5e15bd92e620bef172e4378704bca48dad
Frederic-LM <59538680+Frederic-LM@users.noreply.github.com>
2025-09-08 09:00:37 +0200

    Update utils.py

--- bangbang_engine/utils.py
+++ bangbang_engine/utils.py
@@ -10,10 +10,12 @@ from pathlib import Path
 from typing import Optional, Set, Tuple
 
 def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         base_path = Path(sys._MEIPASS)
     except Exception:
-        base_path = Path(__file__).parent.absolute()
+        base_path = Path(sys.argv[0]).parent.absolute()
+        
     return base_path / relative_path
 
 def send_notification(title, message, app_name="SortMeDown"):
