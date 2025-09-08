23b8f2b8d4aaefb9c80e50de1309ed9dbebb0be6
Frederic-LM <59538680+Frederic-LM@users.noreply.github.com>
2025-09-08 08:55:46 +0200

    Create __init__.py

---
+++ bangbang_engine/__init__.py
@@ -0,0 +1,12 @@
+# bangbang_engine/__init__.py
+
+# Expose the main classes and enums at the top level of the package
+# This allows the GUI to import them with `import bangbang_engine as backend`
+# and access them like `backend.MediaSorter`, `backend.MediaType`, etc.
+
+from .bangbang import MediaSorter
+from .api import APIClient
+from .config import Config
+from .file_manager import FileManager
+from .models import MediaType, MediaInfo
+from .utils import TitleCleaner
